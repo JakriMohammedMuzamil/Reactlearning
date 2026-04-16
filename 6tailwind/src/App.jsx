eefc8be61052151a6ccf3a00490036774bd7cb4b
JakriMohammedMuzamil <mohdmuzza11@gmail.com>
2026-04-16 11:56:15 +0530

    tailwind basic

---
+++ 6tailwind/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen mx-3 text-xl w-full bg-primary border-4'>App
+
+
+    </div>
+  )
+}
+
+export default App
